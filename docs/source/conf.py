4fa3fb4710346879135b16f774dc7ecbf924d9e6
g1YC31aA <lennartmobil2016@gmail.com>
2019-11-13 14:20:11 +0100

    changed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ dir_path = os.path.join(
     os.pardir,
 )
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath(dir_path))
 
 
 # -- Project information -----------------------------------------------------
